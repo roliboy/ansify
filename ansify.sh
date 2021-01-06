b16f75f726e60905f371d3329318ca39f513b365
roliboy <roliboy@protonmail.com>
2021-01-06 19:55:18 +0200

    argument parser and different methods for handling transparency

--- ansify.sh
+++ ansify.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-filename="$1"
+while [ "$#" -gt 0 ]; do
+case "$1" in
+    -h|--help)
+        echo "usage:"
+        echo "    $0 [options] filename"
+        echo "options:"
+        echo "    -a, --alpha: specify method for handling transparency on filled background and transparent foreground edge case"
+        echo "        duplicate (default): draw the foreground with the same color as the background"
+        echo "        skip: don't draw background either"
+        echo "        <custom>: fill transparency with custom color, where <custom> is a rgb triplet of the form 'r,g,b'"
+        exit 0
+        shift
+        ;;
+    -a|--alpha)
+        alpha_method="$2"
+        if [ "$alpha_method" = duplicate ]; then
+            :
+        elif [ "$alpha_method" = skip ]; then
+            :
+        elif [ "$(awk -F',' '{print NF-1}' <<< $alpha_method)" = 2 ]; then
+            alpha_method="$(tr ',' ';' <<< $alpha_method)"
+        else
+            echo invalid argument for --alpha
+            exit 2
+        fi
+        shift 2
+        ;;
+    *)
+        filename="$1"
+        shift
+        ;;
+esac
+done
+
+[ -z "$filename" ] && echo no file provided && exit 1
+[ -z "$alpha_method" ] && alpha_method="duplicate"
 
 raw=($(convert $filename txt: | sed 's/(\|)//g' | tr ',' ';'))
 
@@ -20,7 +55,6 @@ echo -ne "\e[?25l"
 for y in $(seq 0 $(expr $height / 2 - 1)); do
     ybg="$(expr $y \* 2)"
     yfg="$(expr $y \* 2 + 1)"
-    echo -ne '  '
     for x in $(seq 0 $(expr $width - 1)); do
         bgcolor=${pixels["$x;$ybg:"]}
         fgcolor=${pixels["$x;$yfg:"]}
@@ -31,12 +65,19 @@ for y in $(seq 0 $(expr $height / 2 - 1)); do
         fgrgb="$(cut -d ';' -f1-3 <<< $fgcolor)"
         fga="$(cut -d ';' -f4 <<< $fgcolor)"
 
-        echo -ne "\e[0m"
-        [ "$bga" = 0 ] && [ "$fga" = 0 ] && echo -ne ' '
-        [ "$bga" = 0 ] && [ "$fga" != 0 ] && echo -ne "\e[38;2;${fgrgb}m▄"
-        #[ "$bga" != 0 ] && [ "$fga" = 0 ] && echo -ne "\e[38;2;0;0;0m\e[48;2;${bgrgb}m▄"
-        #[ "$bga" != 0 ] && [ "$fga" = 0 ] && echo -ne ' '
-        [ "$bga" != 0 ] && [ "$fga" = 0 ] && echo -ne "\e[38;2;${bgrgb}m\e[48;2;${bgrgb}m▄"
+        if [ "$alpha_method" = duplicate ]; then
+            [ "$bga" = 0 ] && [ "$fga" = 0 ] && echo -ne "\e[0m "
+            [ "$bga" = 0 ] && [ "$fga" != 0 ] && echo -ne "\e[0m\e[38;2;${fgrgb}m▄"
+            [ "$bga" != 0 ] && [ "$fga" = 0 ] && echo -ne "\e[38;2;${bgrgb}m\e[48;2;${bgrgb}m▄"
+        elif [ "$alpha_method" = skip ]; then
+            [ "$bga" = 0 ] && [ "$fga" = 0 ] && echo -ne "\e[0m "
+            [ "$bga" = 0 ] && [ "$fga" != 0 ] && echo -ne "\e[0m\e[38;2;${fgrgb}m▄"
+            [ "$bga" != 0 ] && [ "$fga" = 0 ] && echo -ne "\e[0m "
+        else
+            [ "$bga" = 0 ] && [ "$fga" = 0 ] && echo -ne "\e[38;2;${alpha_method}m\e[48;2;${alpha_method}m▄"
+            [ "$bga" = 0 ] && [ "$fga" != 0 ] && echo -ne "\e[38;2;${fgrgb}m\e[48;2;${alpha_method}m▄"
+            [ "$bga" != 0 ] && [ "$fga" = 0 ] && echo -ne "\e[38;2;${alpha_method}m\e[48;2;${bgrgb}m▄"
+        fi
         [ "$bga" != 0 ] && [ "$fga" != 0 ] && echo -ne "\e[38;2;${fgrgb}m\e[48;2;${bgrgb}m▄"
     done
     echo -e "\e[0m"
